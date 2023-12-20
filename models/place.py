53fa0c420efe92925687b812ccf11964d373f14d
MentalSep <chahidjava@hotmail.com>
2023-12-21 00:34:02 +0100

    add task 10

--- models/place.py
+++ models/place.py
@@ -5,6 +5,14 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -23,6 +31,8 @@ class Place(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", backref="place",
                                cascade="all, delete")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
@@ -34,3 +44,21 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     rList.append(review)
             return rList
+
+        @property
+        def amenities(self):
+            """ Getter for amenities """
+            from models import storage
+            from models.amenity import Amenity
+            aList = []
+            for amenity in storage.all(Amenity).values():
+                if amenity.place_id == self.id:
+                    aList.append(amenity)
+            return aList
+
+        @amenities.setter
+        def amenities(self, obj):
+            """ Setter for amenities """
+            from models.amenity import Amenity
+            if type(obj) is Amenity:
+                self.amenity_ids.append(obj.id)
