0081ff5f2e29695d79a40eee5c76bfad7fcac1dd
MentalSep <chahidjava@hotmail.com>
2024-01-11 19:44:32 +0100

    add task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 """Module for web app deployment"""
-from fabric.api import put, run, env, local
-from os.path import exists
-from datetime import datetime
+from fabric.api import put, run, env, local, cd, lcd
+import os
 
 env.hosts = ['18.204.14.56', '35.153.226.169']
 
 
 def do_clean(number=0):
     """clean up old versions"""
-    if number == 0 or number == 1:
-        local("cd versions; ls -t | tail -n +2 | xargs rm -rf")
-        run("cd /data/web_static/releases; ls -t | tail -n +2 | xargs rm -rf")
-    else:
-        local("cd versions; ls -t | tail -n +{} | xargs rm -rf".format(number))
-        run("cd /data/web_static/releases; ls -t | tail -n +{} | xargs rm -rf".
-            format(number))
+    number = int(number)
+    if number == 0:
+        number = 1
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
