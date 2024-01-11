3bd8c99294f2f267b77ba94322b4bfa3c2296122
MentalSep <chahidjava@hotmail.com>
2024-01-11 17:52:04 +0100

    add task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ echo "Testing!" >> /data/web_static/releases/test/index.html
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chmod -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 
 sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 
